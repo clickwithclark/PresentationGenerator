d5ca32afc6541ca7f98194a7817e7c1d21e50786
mmavliev <46608486+mmavliev@users.noreply.github.com>
2020-08-17 20:50:18 +0300

    Add files via upload

--- presentationgenerator.com/generated.php
+++ presentationgenerator.com/generated.php
@@ -1,7 +1,7 @@
 <?php
 include_once("config.php");
 $title = str_replace("_"," ",$_GET["title"]);
-$sql = 'SELECT thumb_url FROM `rupresentations` WHERE title="'.$title.'"';
+$sql = 'SELECT thumb_url FROM presentations WHERE title="'.$title.'"';
 $result = mysqli_query($link, $sql);
 
 while ($row = mysqli_fetch_array($result)) {
@@ -9,21 +9,17 @@ while ($row = mysqli_fetch_array($result)) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="ru">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Скачать готовую презентацию <?php echo str_replace("_"," ",$_GET["title"])?> можно абсолютно бесплатно или отправить на любую почту онлайн. Генератор презентаций - презентации на любую тему онлайн и бесплатно!">
-    <?php if($url) {?>
+    <meta name="description" content="Presentation about <?php echo $title ?> by Presentation Generator">
     <meta property="og:image" content="<?php echo $url ?>">
-    <?php } else { ?>
-    <meta property="og:image" content="https://presentationgenerator.ru/images/logoshare.png">
-    <?php } ?>
-    <title>Готовая презентация <?php echo $title ?> скачать бесплатно | Генератор Презентаций</title>
+    <title><?php echo $title ?> | Presentation Generator</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
         integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://presentationgenerator.com/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -41,16 +37,16 @@ while ($row = mysqli_fetch_array($result)) {
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="tutorial.php">Обучение</a>
+                    <a class="nav-link" href="tutorial.php">Tutorial</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="https://ru.wikipedia.org/wiki/<?php echo $_GET["title"]; ?>"
-                        target="_blank">Статья Википедии</a>
+                    <a class="nav-link" href="https://en.wikipedia.org/wiki/<?php echo $_GET["title"]; ?>"
+                        target="_blank">Wikipedia Article</a>
                 </li>
             </ul>
         </div>
     </nav>
-
+    </div>
     <div class="w-100 container main py-3">
         <div class="w-100 d-flex justify-content-center mb-3">
             <div class="description px-3 py-3 w-100">
@@ -78,7 +74,7 @@ while ($row = mysqli_fetch_array($result)) {
                             <div class="w-100 space"></div>
                             <div class="col-sm">
                                 <a href="#" class="link3 download">
-                                    <h3>Текст</h3>
+                                    <h3>Text</h3>
                                 </a>
                             </div>
                             <div class="col-sm mar">
@@ -88,7 +84,7 @@ while ($row = mysqli_fetch_array($result)) {
                             </div>
                             <div class="col-sm">
                                 <a class="download" id="openModal">
-                                    <h4>Отправить на почту</h4>
+                                    <h4>Send by email</h4>
                                 </a>
                             </div>
                         </div>
@@ -96,13 +92,13 @@ while ($row = mysqli_fetch_array($result)) {
                     <div class="progress mt-3">
                         <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0"
                             aria-valuemax="100">
-                            Презентация генерируется...</div>
+                            Presentation is generating...</div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <p>Скачать готовую презентацию "<?php echo str_replace("_"," ",$_GET["title"])?>" можно абсолютно бесплатно или отправить на любую почту онлайн. Просто выберите один из 5 форматов:</p> 
+        <p>You can download premade presentation "<?php echo str_replace("_"," ",$_GET["title"])?>" absolutely free or send it by email. Just choose one of the 5 formats:</p> 
         <ul>
         <li><b>pptx (PowerPoint)</b>
         <li><b>Google Slides</b></li>
@@ -111,21 +107,12 @@ while ($row = mysqli_fetch_array($result)) {
         <li><b>.odp (ODP)</b></li>
     </ul>
     </div>
-    <div class="jumbotron error">
-        <h3 class="display-4">Ошибка</h3>
-        <p class="lead">Неизвестная ошибка</p>
-        <hr class="my-4">
-        <p>Попробуйте ещё раз или выберите другую статью:</p>
-        <button class="btn btn-dark" onClick="window.location.reload();" role="button">Попробовать ещё
-            раз</button>&nbsp;&nbsp;
-        <a class="btn btn-secondary" href="index.php" role="button">Найти другую презентацию</a>
-    </div>
     <div class="py-3">
         <div class="container">
-            <h3>Другие презентации:</h3>
+            <h3>Check out some other presentations:</h3>
             <div class="random-presentations">
                 <?php
-        $sql2 = 'SELECT * FROM `rupresentations` ORDER BY RAND() LIMIT 20';
+        $sql2 = 'SELECT * FROM `presentations` ORDER BY RAND() LIMIT 20';
         $result2 = mysqli_query($link, $sql2);
         while ($row2 = mysqli_fetch_array($result2)) { ?>
                 <a href="../<?php echo str_replace(" ","_",$row2["title"]); ?>">
@@ -140,69 +127,66 @@ while ($row = mysqli_fetch_array($result)) {
     </div>
 
     <div class="modal fade error-modal" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="modal" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Error report</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="form-group">
+    <label for="exampleFormControlTextarea1">Describe error</label>
+    <textarea class="form-control" id="error-description" rows="3" placeholder="slide number, object..."></textarea>
+  </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" id="submitError">Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="emailModal" tabindex="-1" role="dialog" aria-labelledby="emailModal" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Сообщить об ошибке</h5>
+                    <h5 class="modal-title" id="exampleModalCenterTitle">Send presentation by email</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
                     <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Опишите ошибку</label>
-                        <textarea class="form-control" id="error-description" rows="3"
-                            placeholder="номер слайда, объект"></textarea>
+                        <label for="exampleFormControlTextarea1">Enter the email. Download links will be sent to this email.</label>
+                        <input class="form-control" id="email" rows="1" placeholder="Email">
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
-                    <button type="button" class="btn btn-primary" id="submitError">Отправить</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" id="sendEmail">Send</button>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="modal fade" id="emailModal" tabindex="-1" role="dialog" aria-labelledby="emailModal" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalCenterTitle">Отправить презентацию на почту</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Введите электронную почту. На неё придёт письмо со
-                            всеми ссылками на скачивание</label>
-                        <input class="form-control" id="email" rows="1" placeholder="Почта">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
-                    <button type="button" class="btn btn-primary" id="sendEmail">Отправить</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <?php include_once("footer.php"); ?>
+<?php include_once("footer.php"); ?>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
         integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
     </script>
     <script src="js/variables.js"></script>
-    <script src="https://presentationgenerator.com/js/basicfunctions.js"></script>
-    <script src="https://presentationgenerator.com/js/images.js"></script>
-    <script src="https://presentationgenerator.com/js/texts.js"></script>
-    <script src="https://presentationgenerator.com/js/googlejson.js"></script>
-    <script src="https://presentationgenerator.com/js/sorters.js"></script>
+    <script src="js/basicfunctions.js"></script>
+    <script src="js/images.js"></script>
+    <script src="js/texts.js"></script>
+    <script src="js/googlejson.js"></script>
+    <script src="js/sorters.js"></script>
     <script src="js/infogetters.js"></script>
     <script src="https://presentationgenerator.com/js/creation.js"></script>
     <script src="js/index-gen.js"></script>
-    <script src="https://presentationgenerator.com/js/a.js"></script>
+    <script src="js/a.js"></script>
     <script src="https://apis.google.com/js/api.js"></script>
     <script src="js/googleapi.js"></script>
 </body>
--- presentationgenerator.com/index.php
+++ presentationgenerator.com/index.php
@@ -1,33 +1,25 @@
-
 <?php
 include_once("config.php");
 ?>
 
 <!DOCTYPE html>
-<html lang="ru">
-<head>
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-164180494-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+<html lang="en">
 
-  gtag('config', 'UA-164180494-1');
-</script>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Бесплатный онлайн генератор презентаций. Скачать готовую презентацию по любой теме теперь быстро и бесплатно!">
-    <meta property="og:image" content="https://presentationgenerator.ru/images/logoshare.png">
-    <title>Бесплатный Генератор Презентаций - презентации на любую тему онлайн!</title>
+    <meta name="description" content="Free online presentation generator! Download premade presentation is now easy and free! You can dowload premade slides in pptx(PowerPoint), Google Slides, .pdf (PDF), .txt and .odp (ODP) formats!">
+    <meta property="og:image" content="https://presentationgenerator.ru/images/logo.png">
+    <title>Presentation Generator - download premade presentation on any topic!</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
         integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://presentationgenerator.com/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg sticky-top">
         <a class="navbar-brand" href="index.php">
-            <img src="images/logo.png" height="50" alt="Логотип Генератор Презентаций">
+            <img src="images/logo.png" height="50">
         </a>
         <button class="btn navbar-icon not-active navbar-toggler" type="button" data-toggle="collapse"
             data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -38,10 +30,10 @@ include_once("config.php");
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="tutorial.php">Обучение</a>
+                    <a class="nav-link" href="tutorial.php">Tutorial</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="https://presentationgenerator.com">ENG</a>
+                    <a class="nav-link" href="https://presentationgenerator.ru">РУС</a>
                 </li>
             </ul>
         </div>
@@ -49,12 +41,12 @@ include_once("config.php");
 
     <section class="container starting py-3">
         <div class="w-100 ">
-            <h1 class="text-center">Генератор Презентаций</h1>
+            <h1 class="text-center">Presentation Generator</h1>
             <div class="input-group mt-3">
-                <input type="text" class="form-control" id="mainInput" placeholder="Введите тему презентации"
+                <input type="text" class="form-control" id="mainInput" placeholder="Enter theme of presentation"
                     name="task">
                 <div class="input-group-prepend">
-                    <button type="button" class="btn btn-dark" id="submit" aria-label="Найти презентацию"> 
+                    <button type="button" class="btn btn-dark" id="submit">
                         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
                             id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512"
                             style="enable-background:new 0 0 512 512;" xml:space="preserve" width="20px" height="20px"
@@ -80,24 +72,26 @@ include_once("config.php");
     </section>
     <section class="dark-back py-3">
         <div class="container">
-            <h2 class="text-left">На любую тему.</h2>
+            <h2 class="text-left">Pick up any topic.</h2>
             <div class="google-slides-container">
-                <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSPNZ2ejIrvKgpOPtms-jAybejHq9i65mH_d185ICnGqolUcHe2XcPSRWXHUBqqmJWJa394c4Gb4LBp/embed?start=false&loop=false&delayms=3000" frameborder="0" width="100%" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"  id="#if"></iframe>
-
-
+                <iframe
+                    src="https://docs.google.com/presentation/d/e/2PACX-1vTU4i51mWMH_R0wmUdNhduWHjex5vg_B8eTfpRuSSAJ9Kbzzi8cD-UKTopbdfWgW7WjMI-oTFhEY_5S/embed?start=false&loop=false&delayms=3000"
+                    frameborder="0" width="100%" height="569" allowfullscreen="true" mozallowfullscreen="true"
+                    webkitallowfullscreen="true" id="#if"></iframe>
             </div>
             <br>
             <div class="row">
-            <div class="col-md-8">
-                    <p>Это генератор презентаций по тексту статей Википедии! Вы можете скачать <b>Google Slides</b> презентацию, а затем конвертировать в <b>PowerPoint</b>, PDF или <b>Keynote</b>. Просто введите тему и скачайте презентацию!
+                <div class="col-md-8">
+                    <p>This is online Presentation Generator. You can generate and download prepared presentations on any topic! You can download <b>Google Slides</b>, 
+                        <b>PowerPoint</b>, <b>PDF</b> or <b>Keynote</b>. Just enter the topic and download slides!
                     </p>
                 </div>
                 <div class="col-md-4">
-                    <a href="/Культура_Древней_Руси" class="btn btn-light">Скачать эту презентацию</a>
-                    <button class="btn btn-primary generate-btn navbar-text mt-3" aria-label="Сгенерировать свою презентацию">Сгенерировать свою
+                    <a href="/Alexander_the_Great" class="btn btn-light">Get this presentation</a>
+                    <button class="btn btn-primary generate-btn navbar-text mt-3">Generate your one
 
-                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
-                            xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 326.387 326.387"
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                            x="0px" y="0px" viewBox="0 0 326.387 326.387"
                             style="enable-background:new 0 0 326.387 326.387;" xml:space="preserve" height=30px>
                             <polygon
                                 points="252.086,178.355 171.154,144.27 231.9,0 74.301,148.018 155.236,182.104 94.487,326.387 " />
@@ -140,19 +134,18 @@ include_once("config.php");
         </div>
     </section>
     <section class="container pt-3">
-    <h2 class="text-center">Скачайте, Измените, Выступите.</h2>
-        <p>Возьмите готовую презентацию за основу. Удалите ненужное и добавьте недостающее. Выберите тему, отрегулируйте параметры блоков и ваша отличная презентация готова! </p>
-        <img 
-        src="images/change-theme.png" 
-         class="w-100" alt="Генератор презентаций изменить тему">
-
+        <h2 class="text-center">Download, Edit, Show.</h2>
+        <p>Use generated presentation as a base. Delete extra stuff and add what's missing. Choose theme, adjust blocks
+            and your awesome presentation is ready to be shown!</p>
+        <img src="images/change-theme.png" class="w-100">
         <div class="text-center py-3">
             <div>
-                <a href="/Белая_акула" class="btn btn-dark mb-3">Скачать эту классную презентацию про акул</a>
-                <button class="btn btn-primary generate-btn navbar-text ml-3 mb-3" aria-label="Сгенерировать свою презентацию">Сгенерировать свою
-                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
-                        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 326.387 326.387"
-                        style="enable-background:new 0 0 326.387 326.387;" xml:space="preserve" height=30px>
+                <a href="/Giant_Panda" class="btn btn-dark mb-3">Get an awesome presentation about pandas</a>
+                <button class="btn btn-primary generate-btn navbar-text ml-3 mb-3">Generate your one
+
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                        x="0px" y="0px" viewBox="0 0 326.387 326.387" style="enable-background:new 0 0 326.387 326.387;"
+                        xml:space="preserve" height=30px>
                         <polygon
                             points="252.086,178.355 171.154,144.27 231.9,0 74.301,148.018 155.236,182.104 94.487,326.387 " />
                         <g>
@@ -193,28 +186,28 @@ include_once("config.php");
 
     <section class="py-3" style="border-top: 17px solid #ffffff;border-bottom: 17px solid #ffffff;">
         <div class="container">
-        <h3>>1.000.000 возможных презентаций</h2>
-        <p>Самое большое колличество презентаций в Рунете.</p>
-</div>
-<div class="all-presentations">
-                <?php
-        $sql2 = 'SELECT * FROM `rupresentations` ORDER BY `id` DESC LIMIT 90';
+            <h3>>6.000.000 possible presentations</h2>
+                <p>The biggest presentation library in the world.</p>
+        </div>
+        <div class="all-presentations">
+            <?php
+        $sql2 = 'SELECT * FROM `presentations` ORDER BY `id` DESC LIMIT 90';
         $result2 = mysqli_query($link, $sql2);
         $count = 0;
         while ($row2 = mysqli_fetch_array($result2)) { 
             if ($count % 30 == 0 && $count > 0) { ?>
-                </div><div class="all-presentations">
+        </div>
+        <div class="all-presentations">
             <?php } ?>
-                    <div class="single">
-                    <a href="../<?php echo str_replace(" ","_",$row2["title"]); ?>" class="pres-link">
-                        <div><?php echo $row2["title"]; ?></div>
-            </a>
-                    </div>
+            <div class="single">
+                <a href="../<?php echo str_replace(" ","_",$row2["title"]); ?>" class="pres-link">
+                    <div><?php echo $row2["title"]; ?></div>
+                </a>
+            </div>
             <?php
             $count++;
             } ?>
-            </div>
-
+        </div>
     </section>
 
     <?php include_once("footer.php"); ?>
